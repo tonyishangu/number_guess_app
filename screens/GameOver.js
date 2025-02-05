aa4a00e6ac8cd0a2ed02896696a4c8285588ef01
tonyishangu <tonyishangu@gmail.com>
2025-02-05 12:38:40 +0300

    adding nexted tags and styling them

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,43 +1,57 @@
-import { View, Image, StyleSheet, Text } from "react-native"
-import Title from "../components/ui/Title"
-import Colors from "../constants/colors"
+import { View, Image, StyleSheet, Text } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-function GameOverScreen () {
- return (
+function GameOverScreen() {
+  return (
     <View style={styles.rootContainer}>
-        <Title>
-            Game Over
-        </Title>
-        <View style={styles.imageContainer}>
-        <Image style={styles.image} source={require('../assets/images/success.png')} />
-        </View>
-        <Text>
-            Your Phone guessed X times to get the number Y
-        </Text>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text style={styles.summaryText}>
+        Your Phone guessed <Text style={styles.highlight}>X</Text> times to get
+        the number <Text style={styles.highlight}>Y</Text>
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
- )
+  );
 }
 
-export default GameOverScreen
+export default GameOverScreen;
 
 const styles = StyleSheet.create({
-    imageContainer: {
-        borderRadius: 150,
-        width: 300,
-        height:300,
-        borderWidth: 3,
-        borderColor: Colors.primary800,
-        overflow: 'hidden',
-        margin: 36,
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-    },
-    rootContainer: {
-        flex: 1,
-        padding: 24,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
+  },
+});
